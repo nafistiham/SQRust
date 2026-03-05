5b3124678441be995a72a56a4f5bc50ef7c05964
Nafis Tiham <nafistiham@gmail.com>
2026-03-05 15:18:51 +0600

    feat(cli): register all Wave 4 rules (35 rules total)

--- sqrust-cli/src/main.rs
+++ sqrust-cli/src/main.rs
@@ -3,8 +3,10 @@ use rayon::prelude::*;
 use sqrust_core::{FileContext, Rule};
 use sqrust_rules::ambiguous::group_by_position::GroupByPosition;
 use sqrust_rules::ambiguous::having_without_group_by::HavingWithoutGroupBy;
+use sqrust_rules::ambiguous::implicit_cross_join::ImplicitCrossJoin;
 use sqrust_rules::ambiguous::order_by_position::OrderByPosition;
 use sqrust_rules::ambiguous::select_star_with_other_columns::SelectStarWithOtherColumns;
+use sqrust_rules::ambiguous::unaliased_expression::UnaliasedExpression;
 use sqrust_rules::capitalisation::functions::Functions;
 use sqrust_rules::capitalisation::keywords::Keywords;
 use sqrust_rules::capitalisation::literals::Literals;
@@ -13,17 +15,25 @@ use sqrust_rules::convention::coalesce::Coalesce;
 use sqrust_rules::convention::comma_style::CommaStyle;
 use sqrust_rules::convention::count_star::CountStar;
 use sqrust_rules::convention::distinct_parenthesis::DistinctParenthesis;
+use sqrust_rules::convention::in_null_comparison::InNullComparison;
 use sqrust_rules::convention::is_null::IsNull;
 use sqrust_rules::convention::not_equal::NotEqual;
 use sqrust_rules::convention::select_star::SelectStar;
+use sqrust_rules::convention::trailing_comma::TrailingComma;
 use sqrust_rules::layout::long_lines::LongLines;
 use sqrust_rules::layout::single_space_after_comma::SingleSpaceAfterComma;
+use sqrust_rules::layout::space_before_comma::SpaceBeforeComma;
 use sqrust_rules::layout::tab_indentation::TabIndentation;
+use sqrust_rules::layout::trailing_blank_lines::TrailingBlankLines;
 use sqrust_rules::layout::trailing_newline::TrailingNewline;
 use sqrust_rules::layout::trailing_whitespace::TrailingWhitespace;
+use sqrust_rules::lint::delete_without_where::DeleteWithoutWhere;
 use sqrust_rules::lint::duplicate_alias::DuplicateAlias;
 use sqrust_rules::lint::unused_cte::UnusedCte;
+use sqrust_rules::lint::update_without_where::UpdateWithoutWhere;
+use sqrust_rules::structure::column_count::ColumnCount;
 use sqrust_rules::structure::limit_without_order_by::LimitWithoutOrderBy;
+use sqrust_rules::structure::nested_subquery::NestedSubquery;
 use sqrust_rules::structure::union_all::UnionAll;
 use std::path::PathBuf;
 use std::process;
@@ -55,8 +65,10 @@ fn rules() -> Vec<Box<dyn Rule>> {
         // Layout
         Box::new(TrailingWhitespace),
         Box::new(TrailingNewline),
+        Box::new(TrailingBlankLines),
         Box::new(TabIndentation),
         Box::new(SingleSpaceAfterComma),
+        Box::new(SpaceBeforeComma),
         Box::new(LongLines::default()),
         // Capitalisation
         Box::new(Keywords),
@@ -72,17 +84,25 @@ fn rules() -> Vec<Box<dyn Rule>> {
         // Convention (continued)
         Box::new(IsNull),
         Box::new(DistinctParenthesis),
+        Box::new(TrailingComma),
+        Box::new(InNullComparison),
         // Ambiguous
         Box::new(GroupByPosition),
         Box::new(OrderByPosition),
         Box::new(SelectStarWithOtherColumns),
         Box::new(HavingWithoutGroupBy),
+        Box::new(ImplicitCrossJoin),
+        Box::new(UnaliasedExpression),
         // Lint
         Box::new(UnusedCte),
         Box::new(DuplicateAlias),
+        Box::new(DeleteWithoutWhere),
+        Box::new(UpdateWithoutWhere),
         // Structure
         Box::new(UnionAll),
         Box::new(LimitWithoutOrderBy),
+        Box::new(NestedSubquery::default()),
+        Box::new(ColumnCount::default()),
     ]
 }
 
