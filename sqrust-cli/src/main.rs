f837afde72378439080f268644a9f72d782f5009
Nafis Tiham <nafistiham@gmail.com>
2026-03-05 15:31:09 +0600

    feat(cli): wire 10 Wave 5 rules (45 rules total)
    Add to CLI: CaseElse, OrderByWithOffset, NegatedIsNull, DuplicateCteNames,
    DistinctGroupBy, SubqueryInSelect, TableAliasConflict, JoinWithoutCondition,
    CommentSpacing, SpaceAroundEquals.

--- sqrust-cli/src/main.rs
+++ sqrust-cli/src/main.rs
@@ -4,13 +4,16 @@ use sqrust_core::{FileContext, Rule};
 use sqrust_rules::ambiguous::group_by_position::GroupByPosition;
 use sqrust_rules::ambiguous::having_without_group_by::HavingWithoutGroupBy;
 use sqrust_rules::ambiguous::implicit_cross_join::ImplicitCrossJoin;
+use sqrust_rules::ambiguous::join_without_condition::JoinWithoutCondition;
 use sqrust_rules::ambiguous::order_by_position::OrderByPosition;
 use sqrust_rules::ambiguous::select_star_with_other_columns::SelectStarWithOtherColumns;
+use sqrust_rules::ambiguous::table_alias_conflict::TableAliasConflict;
 use sqrust_rules::ambiguous::unaliased_expression::UnaliasedExpression;
 use sqrust_rules::capitalisation::functions::Functions;
 use sqrust_rules::capitalisation::keywords::Keywords;
 use sqrust_rules::capitalisation::literals::Literals;
 use sqrust_rules::capitalisation::types::Types;
+use sqrust_rules::convention::case_else::CaseElse;
 use sqrust_rules::convention::coalesce::Coalesce;
 use sqrust_rules::convention::comma_style::CommaStyle;
 use sqrust_rules::convention::count_star::CountStar;
@@ -18,10 +21,13 @@ use sqrust_rules::convention::distinct_parenthesis::DistinctParenthesis;
 use sqrust_rules::convention::in_null_comparison::InNullComparison;
 use sqrust_rules::convention::is_null::IsNull;
 use sqrust_rules::convention::not_equal::NotEqual;
+use sqrust_rules::convention::order_by_with_offset::OrderByWithOffset;
 use sqrust_rules::convention::select_star::SelectStar;
 use sqrust_rules::convention::trailing_comma::TrailingComma;
+use sqrust_rules::layout::comment_spacing::CommentSpacing;
 use sqrust_rules::layout::long_lines::LongLines;
 use sqrust_rules::layout::single_space_after_comma::SingleSpaceAfterComma;
+use sqrust_rules::layout::space_around_equals::SpaceAroundEquals;
 use sqrust_rules::layout::space_before_comma::SpaceBeforeComma;
 use sqrust_rules::layout::tab_indentation::TabIndentation;
 use sqrust_rules::layout::trailing_blank_lines::TrailingBlankLines;
@@ -29,11 +35,15 @@ use sqrust_rules::layout::trailing_newline::TrailingNewline;
 use sqrust_rules::layout::trailing_whitespace::TrailingWhitespace;
 use sqrust_rules::lint::delete_without_where::DeleteWithoutWhere;
 use sqrust_rules::lint::duplicate_alias::DuplicateAlias;
+use sqrust_rules::lint::duplicate_cte_names::DuplicateCteNames;
+use sqrust_rules::lint::negated_is_null::NegatedIsNull;
 use sqrust_rules::lint::unused_cte::UnusedCte;
 use sqrust_rules::lint::update_without_where::UpdateWithoutWhere;
 use sqrust_rules::structure::column_count::ColumnCount;
+use sqrust_rules::structure::distinct_group_by::DistinctGroupBy;
 use sqrust_rules::structure::limit_without_order_by::LimitWithoutOrderBy;
 use sqrust_rules::structure::nested_subquery::NestedSubquery;
+use sqrust_rules::structure::subquery_in_select::SubqueryInSelect;
 use sqrust_rules::structure::union_all::UnionAll;
 use std::path::PathBuf;
 use std::process;
@@ -70,6 +80,8 @@ fn rules() -> Vec<Box<dyn Rule>> {
         Box::new(SingleSpaceAfterComma),
         Box::new(SpaceBeforeComma),
         Box::new(LongLines::default()),
+        Box::new(CommentSpacing),
+        Box::new(SpaceAroundEquals),
         // Capitalisation
         Box::new(Keywords),
         Box::new(Functions),
@@ -81,11 +93,12 @@ fn rules() -> Vec<Box<dyn Rule>> {
         Box::new(Coalesce),
         Box::new(SelectStar),
         Box::new(CountStar),
-        // Convention (continued)
         Box::new(IsNull),
         Box::new(DistinctParenthesis),
         Box::new(TrailingComma),
         Box::new(InNullComparison),
+        Box::new(CaseElse),
+        Box::new(OrderByWithOffset),
         // Ambiguous
         Box::new(GroupByPosition),
         Box::new(OrderByPosition),
@@ -93,16 +106,22 @@ fn rules() -> Vec<Box<dyn Rule>> {
         Box::new(HavingWithoutGroupBy),
         Box::new(ImplicitCrossJoin),
         Box::new(UnaliasedExpression),
+        Box::new(TableAliasConflict),
+        Box::new(JoinWithoutCondition),
         // Lint
         Box::new(UnusedCte),
         Box::new(DuplicateAlias),
         Box::new(DeleteWithoutWhere),
         Box::new(UpdateWithoutWhere),
+        Box::new(NegatedIsNull),
+        Box::new(DuplicateCteNames),
         // Structure
         Box::new(UnionAll),
         Box::new(LimitWithoutOrderBy),
         Box::new(NestedSubquery::default()),
         Box::new(ColumnCount::default()),
+        Box::new(DistinctGroupBy),
+        Box::new(SubqueryInSelect),
     ]
 }
 
